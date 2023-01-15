c6e32c21cab7b0a6ba8bb55880ee70ff7e69a093
Vasiliy Kozyrev <ezqc123@gmail.com>
2023-01-15 18:19:01 +0300

    (("\$@~!0018)((({}[]] Руссо  турсисто!

--- CMake/EnsureGenerationSourceResource.cmake
+++ CMake/EnsureGenerationSourceResource.cmake
@@ -45,44 +45,46 @@ function(ResourceSourceGenerationCustomCommand Target OutDir)
     file(READ ${FileToResource} FileHEX HEX)
     string(
       REGEX REPLACE
-      "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6, 0x\\7, 0x\\8, 0x\\9, \\n" FileHEX ${FileHEX}
-    )
-    string(
-      REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6, 0x\\7, 0x\\8" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6, 0x\\7, 0x\\8, \\n" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6, 0x\\7" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6, 0x\\7" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5, 0x\\6" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3, 0x\\4, 0x\\5" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3, 0x\\4" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3, 0x\\4" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2, 0x\\3" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2, 0x\\3" FileHEX ${FileHEX}
     )
     string(
       REGEX REPLACE
       "([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])"
-      "0x\\1, 0x\\2" FileHEX ${FileHEX}
+      "Y0x\\1, 0x\\2" FileHEX ${FileHEX}
     )
+    string(
+      REGEX REPLACE
+      "\n([0-9a-f][0-9a-f])"
+      "\nY0x\\1" FileHEX ${FileHEX}
+    )
+
+    string(REPLACE "Y" "  " FileHEX ${FileHEX} )
 
     string(CONFIGURE [[
 const char* @FunctionName@()\;
